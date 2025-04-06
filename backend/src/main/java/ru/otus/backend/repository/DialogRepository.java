e84047964f89d89f3e7bec47c113564c581487e7
Zaur Dadaev <zaur.dadaev@pprcard.ru>
2025-04-06 11:40:54 +0300

    Домашняя работа №5

---
+++ backend/src/main/java/ru/otus/backend/repository/DialogRepository.java
@@ -0,0 +1,18 @@
+package ru.otus.backend.repository;
+
+import ru.otus.backend.model.DialogMessage;
+
+import java.util.List;
+
+public interface DialogRepository {
+
+    /**
+     * Сохраняет новое сообщение в базе данных
+     */
+    Long saveMessage(Long fromUserId, Long toUserId, String text, String dialogId);
+
+    /**
+     * Находит все сообщения в диалоге, упорядоченные по времени
+     */
+    List<DialogMessage> findMessagesByDialogId(String dialogId);
+}
